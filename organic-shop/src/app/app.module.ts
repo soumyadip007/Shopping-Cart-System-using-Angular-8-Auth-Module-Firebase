cbed39d9994398c2b2daa610b03f83ef69e8efd1
soumyadip007 <soumyadip.note@gmail.com>
2019-06-19 23:54:44 +0530

    bootstrap comp & navbar added

--- organic-shop/src/app/app.module.ts
+++ organic-shop/src/app/app.module.ts
@@ -7,10 +7,12 @@ import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { environment } from 'src/environments/environment';
+import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BsNavbarComponent
   ],
   imports: [
     BrowserModule,
