81aa8a9f464e07773751ab6ca1c5b07dc39be43b
soumyadip007 <soumyadip.note@gmail.com>
2019-06-19 23:44:14 +0530

    app.comp config

--- organic-shop/src/app/app.module.ts
+++ organic-shop/src/app/app.module.ts
@@ -3,12 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from 'src/environments/environment';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireModule
   ],
   providers: [],
   bootstrap: [AppComponent]
