1466644ca2572229f34d434db35cdc7d797c1c2c
soumyadip007 <soumyadip.note@gmail.com>
2019-06-20 00:21:05 +0530

    Router added

--- organic-shop/src/app/app.module.ts
+++ organic-shop/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { RouterModule } from '@angular/router';
+
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
@@ -14,6 +16,8 @@ import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { CheckOutComponent } from './check-out/check-out.component';
 import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
 import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -22,16 +26,28 @@ import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.componen
     HomeComponent,
     ProductsComponent,
     ShoppingCartComponent,
-    CheckoutComponent,
     CheckOutComponent,
     AdminProductsComponent,
-    AdminOrdersComponent
+    AdminOrdersComponent,
+    OrderSuccessComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    RouterModule.forRoot([
+      {path:'', component: HomeComponent},
+      {path:'products', component: ProductsComponent},
+      {path:'shopping-cart', component: ShoppingCartComponent},
+      {path:'check-out', component: CheckOutComponent},
+      {path:'order-success', component: OrderSuccessComponent},
+      {path:'login', component: LoginComponent},
+      {path:'admin/product', component: AdminProductsComponent},
+      {path:'admin/orders', component: AdminOrdersComponent}
+    ])
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
