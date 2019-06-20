e49cc4ab249f94b7521a7deb043be9c4b663d815
soumyadip007 <soumyadip.note@gmail.com>
2019-06-20 21:18:24 +0530

    categories config

--- shop/src/app/category.service.ts
+++ shop/src/app/category.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  constructor() { }
+  constructor(private db: AngularFireDatabase) { }
+
+  getCategories(){
+    return this.db.list('/categories');
+  }
 }
