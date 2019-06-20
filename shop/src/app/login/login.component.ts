66627cd4111c4a3e6d3cdf4085f282629a411a17
soumyadip007 <soumyadip.note@gmail.com>
2019-06-20 14:08:55 +0530

    Login method added

--- shop/src/app/login/login.component.ts
+++ shop/src/app/login/login.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AngularFireAuth} from 'angularfire2/auth';
+import * as firebase from 'firebase';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent  {
 
-  constructor() { }
+  constructor(private afAuth : AngularFireAuth) {
+  //  firebase.
+   }
 
   ngOnInit() {
+  }
+
+  login(){
+ this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
   }
 
 }
