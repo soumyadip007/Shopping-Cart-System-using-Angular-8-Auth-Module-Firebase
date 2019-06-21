c8331d0171d2a399630ab3204a973f41484c5fc1
soumyadip007 <soumyadip.note@gmail.com>
2019-06-21 23:28:07 +0530

    Bug

--- shop/src/app/shipping-form/shipping-form.component.ts
+++ shop/src/app/shipping-form/shipping-form.component.ts
@@ -1,10 +1,4 @@
-import { ShoppingCart } from './../models/shopping-cart';
-import { OrderService } from './../order.service';
-import { AuthService } from './../auth.service';
-import { Router } from '@angular/router';
-import { Subscription } from 'rxjs/Subscription';
-import { Component, OnInit, OnDestroy, Input } from '@angular/core';
-import { Order } from "../models/order";
+import { ShoppingCart } ];
 
 @Component({
   selector: 'shipping-form',
