16a03b44470ac01cab14bf3a6def509207e606ea
soumyadip007 <soumyadip.note@gmail.com>
2019-06-21 23:08:31 +0530

    Fire removed

--- shop/src/app/app.module.ts
+++ shop/src/app/app.module.ts
@@ -9,9 +9,7 @@ import { AuthService } from './auth.service';
 import { environment } from './../environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AngularFireModule } from 'angularfire2'; 
-import { AngularFireDatabaseModule } from 'angularfire2/database'; 
-import { AngularFireAuthModule } from 'angularfire2/auth'; 
+
 import { RouterModule } from '@angular/router'; 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap'; 
 import { FormsModule } from '@angular/forms'; 
