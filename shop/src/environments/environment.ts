64f69fac896d2bd2a2b1bdd8df4c7b2e562141a6
soumyadip007 <soumyadip.note@gmail.com>
2019-06-21 21:52:12 +0530

    firebase env added

--- shop/src/environments/environment.ts
+++ shop/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyB8bZtoaBTmSpzRlYNxeUP3mskz-Yr9A7w",
+    authDomain: "oshop-angular8.firebaseapp.com",
+    databaseURL: "https://oshop-angular8.firebaseio.com",
+    projectId: "oshop-angular8",
+    storageBucket: "",
+    messagingSenderId: "780703733699",
+    appId: "1:780703733699:web:27564c393302e553"
+  }
 };
 
 /*
